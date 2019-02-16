a46d739c269a2fa8f668bb52189dde23579e20db
Grace <gh971@nyu.edu>
2019-02-16 11:13:23 +0900

    Gyro script and changed some build settings

--- Tele-Room/Assets/Scripts/AccelerometerInput.cs
+++ Tele-Room/Assets/Scripts/AccelerometerInput.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class AccelerometerInput : MonoBehaviour
 {
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+
+    private GameObject cameraContainer;
+    private Quaternion rot;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+
+        gyroEnabled = EnableGyro();
+    }
+
+    private bool EnableGyro()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+
+            cameraContainer.transform.rotation = Quaternion.Euler(90f, 90f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+
+            return true;
+        }
+
+        return false;
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (gyroEnabled)
+        {
+            transform.localRotation = gyro.attitude * rot;
+        }
     }
 }
