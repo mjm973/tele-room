1bd2dfe29142f3c63e3e2cb3e0f0b030deb2913a
Mateo Juvera <mjm973@nyu.edu>
2019-02-16 14:10:05 +0900

    NET WORK WORKS YEA

---
+++ Tele-Room/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class CameraMovement : MonoBehaviourPunCallbacks, IPunObservable
+{
+    Camera cam;
+
+    bool LockedToMaster = true;
+    
+    #region Properties
+
+
+
+    #endregion
+
+
+    #region Base callbacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        if (cam == null) {
+            cam = Camera.main;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+
+    #region PUN Callbacks
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        Debug.Log("merp");
+        if (stream.IsWriting) {
+            Vector3 pos = GetPosition();
+            Quaternion rot = GetRotation();
+            stream.Serialize(ref pos);
+            stream.Serialize(ref rot);
+        } else {
+            Vector3 pos = Vector3.zero;
+            Quaternion rot = Quaternion.identity;
+            stream.Serialize(ref pos);
+            stream.Serialize(ref rot);
+
+            if (!photonView.IsMine) {
+                Debug.Log(string.Format("Received Position ({0})", pos.ToString()));
+                Sync(pos, rot);
+            } else {
+                Debug.Log("View is mine :(");
+            }
+        }
+    }
+
+    void OnPhotonInstantiate(PhotonMessageInfo info) {
+        Debug.Log("Hi");
+        //Debug.Log(string.Format("Instantiated by master? {0}", info.Sender.IsMasterClient));
+    }
+
+    #endregion
+
+    #region Methods
+
+    Vector3 GetPosition() {
+
+        //return transform.position;
+        Vector3 p = Input.mousePosition;
+        p.z = 5;
+
+        p = cam.ScreenToWorldPoint(p);
+
+        return p;
+    }
+
+    Quaternion GetRotation() {
+        return transform.rotation;
+    }
+
+    void Sync(Vector3 pos, Quaternion rot) {
+        if (LockedToMaster) {
+            transform.position = pos;
+            transform.rotation = rot;
+        }
+    }
+
+    #endregion
+}
--- Tele-Room/Assets/Scripts/NetworkManager.cs
+++ Tele-Room/Assets/Scripts/NetworkManager.cs
@@ -1,18 +1,95 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class NetworkManager : MonoBehaviour
+public class NetworkManager : MonoBehaviourPunCallbacks
 {
+
+    string version = "1";
+
+    public GameObject cameraPrefab;
+
+    #region Base Callbacks
+
+    private void Awake() {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        Connect();
     }
+
 
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    #endregion
+
+    #region PUN Callbacks
+
+    public override void OnConnectedToMaster() {
+#if UNITY_EDITOR
+        Debug.Log("Connecting to master...");
+#endif
+
+        base.OnConnectedToMaster();
+
+        Debug.Log(PhotonNetwork.CountOfRooms);
+
+        JoinBaseRoom();
+    }
+
+    public override void OnJoinedRoom() {
+#if UNITY_EDITOR
+        Debug.Log("Found a room!");
+#endif
+        base.OnJoinedRoom();
+
+        Debug.Log(string.Format("Joined {0} with {1} players!", PhotonNetwork.CurrentRoom.Name, PhotonNetwork.CurrentRoom.PlayerCount));
+    }
+
+    public override void OnDisconnected(DisconnectCause cause) {
+#if UNITY_EDITOR
+        Debug.Log("Disconnected!");
+#endif
+        base.OnDisconnected(cause);
+    }
+
+    public override void OnCreatedRoom() {
+        base.OnCreatedRoom();
+
+        Debug.Log("Made a room :)");
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Handle the initial connection to the servers
+    /// </summary>
+    public void Connect() {
+        if (PhotonNetwork.IsConnected) {
+            JoinBaseRoom();
+        } else {
+            PhotonNetwork.GameVersion = version;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public void JoinBaseRoom() {
+        RoomOptions opts = new RoomOptions();
+        opts.IsVisible = true;
+        opts.MaxPlayers = 4;
+        opts.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom("Our Room", opts, TypedLobby.Default);
+
+        PhotonNetwork.Instantiate("NetCam", Vector3.zero, Quaternion.identity);
+    }
 }
+
